99018f46ff8edf24728f85c702604287ebcb896a
Alisson Chiquitto <chiquitto@gmail.com>
2017-04-28 15:31:45 -0300

    3serie - ZF

---
+++ tads-3serie-servicos-internet/zf1-blog/application/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+class IndexController extends Blog_Controller_Action {
+
+    public function indexAction() {
+        
+    }
+
+    public function categoriasAction() {
+        
+    }
+
+    public function postAction() {
+        
+    }
+
+}
